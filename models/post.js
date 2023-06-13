7ae01a65b893b0cc7d4dca4014898e2e80088578
mustafa9934 <mustsfa.khan9434@gmail.com>
2023-06-13 05:09:22 +0200

    create posts model

---
+++ models/post.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  peoples: {
+    type: Number,
+    required: true,
+  },
+  photo: {
+    type: String,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Post", postSchema);
